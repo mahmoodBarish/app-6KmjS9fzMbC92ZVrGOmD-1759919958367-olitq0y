372d9f0ad63f08876d44157156e9bba962125333
mahmoodBarish <45651152+mahmoodBarish@users.noreply.github.com>
2025-10-08 14:11:14 +0300

    feat: add main.dart and configure go_router with all screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'screens/splash_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/onboarding_screen.dart';
+import 'screens/sign_in_screen.dart';
+import 'screens/number_screen.dart';
+import 'screens/verification_screen.dart';
+import 'screens/select_location_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/sign_up_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/explore_screen.dart';
+import 'screens/beverages_screen.dart';
+import 'screens/filters_screen.dart';
+import 'screens/my_cart_screen.dart';
+import 'screens/favorites_screen.dart';
+import 'screens/account_screen.dart';
+import 'screens/error_screen.dart';
+import 'screens/checkout_screen.dart';
+import 'screens/search_screen.dart';
+import 'screens/order_accepted_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final GoRouter _router = GoRouter(
+  initialLocation: '/splash_screen',
+  routes: [
+    GoRoute(
+      path: '/splash_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SplashScreen();
+      },
+    ),
+    GoRoute(
+      path: '/home_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+    ),
+    GoRoute(
+      path: '/onboarding_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const OnboardingScreen();
+      },
+    ),
+    GoRoute(
+      path: '/sign_in_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignInScreen();
+      },
+    ),
+    GoRoute(
+      path: '/number_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const NumberScreen();
+      },
+    ),
+    GoRoute(
+      path: '/verification_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const VerificationScreen();
+      },
+    ),
+    GoRoute(
+      path: '/select_location_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SelectLocationScreen();
+      },
+    ),
+    GoRoute(
+      path: '/login_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginScreen();
+      },
+    ),
+    GoRoute(
+      path: '/sign_up_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SignUpScreen();
+      },
+    ),
+    GoRoute(
+      path: '/product_detail_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const ProductDetailScreen();
+      },
+    ),
+    GoRoute(
+      path: '/explore_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const ExploreScreen();
+      },
+    ),
+    GoRoute(
+      path: '/beverages_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const BeveragesScreen();
+      },
+    ),
+    GoRoute(
+      path: '/filters_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const FiltersScreen();
+      },
+    ),
+    GoRoute(
+      path: '/my_cart_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MyCartScreen();
+      },
+    ),
+    GoRoute(
+      path: '/favorites_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const FavoritesScreen();
+      },
+    ),
+    GoRoute(
+      path: '/account_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const AccountScreen();
+      },
+    ),
+    GoRoute(
+      path: '/error_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const ErrorScreen();
+      },
+    ),
+    GoRoute(
+      path: '/checkout_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const CheckoutScreen();
+      },
+    ),
+    GoRoute(
+      path: '/search_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const SearchScreen();
+      },
+    ),
+    GoRoute(
+      path: '/order_accepted_screen',
+      builder: (BuildContext context, GoRouterState state) {
+        return const OrderAcceptedScreen();
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      routerConfig: _router,
     );
   }
 }
